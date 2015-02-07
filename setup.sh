fe653697d657ba8b51d98c9b5613c5c43c09bb44
vankobe <vankobe@egamimasato-no-MacBook-Pro.local>
2015-02-07 23:47:08 +0900

    add gitconfig

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 DOTFILE_DIR=`dirname "$PWD/$0"`
-DOTFILES=( .vimrc )
+DOTFILES=( .vimrc .gitconfig )
 
-for file in ${DOTFILES}
+for file in ${DOTFILES[@]}
 do
  LINK_COMMAND=`ln -si ${DOTFILE_DIR}/$file $HOME/$file`
  echo ${LINK_COMMAND}
